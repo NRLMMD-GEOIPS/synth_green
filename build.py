a792401daa0a4ce7063acd0105c1fe768c516760
mindyls <mindyls@users.noreply.github.com>
2024-05-02 12:28:24 -0700

    1.12.2 release NRLMMD-GEOIPS/geoips#529

--- build.py
+++ build.py
@@ -16,13 +16,25 @@
 
 from subprocess import run
 from os.path import dirname
+from pathlib import Path
+
+
+def run_untar(setup_kwargs):
+    """Build dependencies."""
+    retval = run(["tar", "-xzvf", Path(dirname(__file__), "dat.tgz")])
+    if retval.returncode != 0:
+        exit(retval.returncode)
+    return setup_kwargs
 
 
 def run_make(setup_kwargs):
     """Build dependencies."""
-    run(["make", "-C", dirname(__file__)])
+    retval = run(["make", "-C", dirname(__file__)])
+    if retval.returncode != 0:
+        exit(retval.returncode)
     return setup_kwargs
 
 
 if __name__ == "__main__":
     run_make({})
+    run_untar({})
